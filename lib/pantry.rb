9b09d26ced3ee21213e8bde4bd40d5959549b5b7
Padraic O'Neill <pconeill@gmail.com>
2017-04-18 09:57:39 -0600

    tests passing, working on testing Pantry#restock

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,19 @@
+require 'pry'
 class Pantry
+  attr_reader :stock
 
+  def initialize
+    @stock = {}
+  end
+
+  def stock_check(item)
+    return 0 if stock.has_key?(item) == false
+
+    stock[item]
+  end
+
+  def restock(item, num)
+    current_num = @stock[item]
+    @stock[item] = num
+  end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,7 +1,33 @@
 require './lib/pantry'
 require 'minitest/autorun'
 require 'minitest/pride'
-
+require 'pry'
 class PantryTest < Minitest::Test
+
+  def test_it_exists
+    pantry = Pantry.new
+
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_pantry_stock
+    pantry = Pantry.new
+
+    assert_equal Hash.new, pantry.stock
+  end
+
+  def test_stock_check
+    pantry = Pantry.new
+
+    assert_equal 0, pantry.stock_check("Cheese")
+  end
+
+  def test_restock
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+
+    assert_equal 10, pantry.stock_check("Cheese")
+  end
+
 
 end
